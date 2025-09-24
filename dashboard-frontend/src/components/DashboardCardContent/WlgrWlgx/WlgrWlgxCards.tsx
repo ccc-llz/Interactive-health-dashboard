2da22d548ec72f6380e539625b4b82cfde12ce10
Leo Zhang <hzha0986@uni.sydney.edu.au>
2025-09-25 01:03:40 +1000

    Stylished WLGR&WLGX Card

--- dashboard-frontend/src/components/DashboardCardContent/WlgrWlgx/WlgrWlgxCards.tsx
+++ dashboard-frontend/src/components/DashboardCardContent/WlgrWlgx/WlgrWlgxCards.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { apiClient } from "../../../service/axios";
+import { X } from 'lucide-react'
 
 const TITLES = [
   { key: "wlgr625", label: "WLGR 625" },
@@ -18,21 +19,22 @@ export default function WlgrWlgxCards() {
   }, []);
 
   return (
-    <div style={{ display: "grid", gap: 16,
-      gridTemplateColumns: "1fr 1fr",
-      gridTemplateRows: "1fr 1fr",
-    }}>
+    <div className="w-full h-full grid grid-rows-2 grid-cols-2 gap-5">
       {TITLES.map(({ key, label }) => (
-        <div key={key} style={{
-          border: "1px solid #ccc",
-          borderRadius: 8,
-          padding: 16,
-          minWidth: 100,
-          textAlign: "center",
-          background: "#f9f9f9"
-        }}>
-          <div style={{ fontWeight: "bold", marginBottom: 8 }}>{label}</div>
-          <div style={{ fontSize: 20 }}>
+        <div key={key} className="relative flex flex-col items-center justify-center bg-gradient-to-b from-white to-gray-100 rounded-xl outline-1 outline-white/30 shadow-xl">
+          <div className="absolute w-full h-full p-2">
+            {['top-2 left-2',
+              'top-2 right-2',
+              'bottom-2 left-2',
+              'bottom-2 right-2'].map((item) => (
+                <div className={`absolute w-2 h-2 rounded-full bg-gradient-to-br from-gray-400 to-gray-600 ${item} flex flex-col items-center justify-center`}>
+                  <X className="w-1.5 h-1.5 bg-gradient-to-b from-gray-800 to-gray-500 bg-clip-text"/>
+                </div>
+              )
+              )}
+          </div>
+          <div className="font-mono" style={{ fontWeight: "bold" }}>{label}</div>
+          <div className="font-mono bg-gradient-to-br from-green-950 to-green-900 px-2 py-1 rounded-lg inset-shadow-sm/40 text-white" style={{ fontSize: 20 }}>
             {typeof values[key] === "number" ? values[key].toFixed(2) : "—"}
           </div>
         </div>
